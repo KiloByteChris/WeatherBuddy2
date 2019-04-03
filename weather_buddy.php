16dfc6633b1b3ff91f93bf0c4cef76479058ff0e
c.mcguire <cdmcguire000@gmail.com>
2019-04-02 20:38:17 -0700

    a fresh start

--- weather_buddy.php
+++ weather_buddy.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Plugin Name:  MyPlugin
-Description:  Example plugin for the video tutorial series, "WordPress: Plugin Development", available at Lynda.com.
-Plugin URI:   https://profiles.wordpress.org/specialk
-Author:       Jeff Starr
+Plugin Name:  Weather Buddy
+Description:  A plugin that displays a weather forecast.
+Plugin URI:   new-volume.com
+Author:       Chris McGuire
 Version:      1.0
-Text Domain:  myplugin
+Text Domain:  weather_buddy
 Domain Path:  /languages
 License:      GPL v2 or later
 License URI:  https://www.gnu.org/licenses/gpl-2.0.txt
@@ -14,32 +14,4 @@ License URI:  https://www.gnu.org/licenses/gpl-2.0.txt
 // exit if file is called directly
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
-}
-
-// if admin area
-if (is_admin()) {
-    // include dependencies
-    require_once plugin_dir_path(__FILE__) . 'admin/admin-menu.php';
-    require_once plugin_dir_path(__FILE__) . 'admin/settings-page.php';
-    require_once plugin_dir_path(__FILE__) . 'admin/settings-register.php';
-    require_once plugin_dir_path(__FILE__) . 'admin/settings-callbacks.php';
-    require_once plugin_dir_path(__FILE__) . 'admin/settings-validate.php';
-}
-
-// include dependencies: admin and public
-require_once plugin_dir_path(__FILE__) . 'includes/core-functions.php';
-
-// default plugin options
-function myplugin_options_default() {
-
-	return array(
-		'custom_url'     => 'https://wordpress.org/',
-		'custom_title'   => 'Powered by WordPress',
-		'custom_style'   => 'disable',
-		'custom_message' => '<p class="custom-message">My custom message</p>',
-		'custom_footer'  => 'Special message for users',
-		'custom_toolbar' => false,
-		'custom_scheme'  => 'default',
-	);
-
 }
